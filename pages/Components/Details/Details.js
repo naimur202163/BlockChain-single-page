80f0e7c9acad139472ea88e1be88ba3e4840ab93
unknown <you@example.com>
2022-01-03 15:37:09 +0600

    almost done

--- pages/Components/Details/Details.js
+++ pages/Components/Details/Details.js
@@ -1,6 +1,8 @@
 import { Box, Button, Container, Grid, Typography } from "@mui/material";
 import React from "react";
 import logo from "../../img/Image.png";
+import NetworkCellIcon from "@mui/icons-material/NetworkCell";
+import StarOutlineIcon from "@mui/icons-material/StarOutline";
 const Details = () => {
   return (
     <div>
@@ -9,12 +11,12 @@ const Details = () => {
           <Grid lg={8} md={8}>
             <Box>
               <div>
-                <Button>Click here</Button>
+                <Button variant="contained">&#8592;</Button>
               </div>
               {/* Heading Text */}
               <div>
                 <Typography variant="h3">Blockchain Simplified</Typography>
-                <p>
+                <p variant="body1">
                   Amet minim mollit non deserunt ullamco est sit aliqua dolor do
                   amet sint. Velit officia consequat duis enim velit mollit.
                   Exercitation veniam consequat sunt nostrud amet.Amet minim
@@ -31,7 +33,9 @@ const Details = () => {
               <div>
                 <div>
                   <div>
-                    <Typography>Technologies Covered</Typography>
+                    <Typography fontWeight="fontWeightBold">
+                      Technologies Covered
+                    </Typography>
                   </div>
                   <div>
                     <Grid sx={{ mt: 4, ml: 1 }} container spacing={2}>
@@ -45,28 +49,28 @@ const Details = () => {
                       <Grid md={2} lg={2}>
                         <div className="box">
                           <Typography sx={{ textAlign: "center" }}>
-                            C++
+                            Blockchain
                           </Typography>
                         </div>
                       </Grid>
                       <Grid md={2} lg={2}>
                         <div className="box">
                           <Typography sx={{ textAlign: "center" }}>
-                            C++
+                            Smart Contracts
                           </Typography>
                         </div>
                       </Grid>
                       <Grid md={2} lg={2}>
                         <div className="box">
                           <Typography sx={{ textAlign: "center" }}>
-                            C++
+                            Blockchain Architecture
                           </Typography>
                         </div>
                       </Grid>
                       <Grid md={2} lg={2}>
                         <div className="box">
                           <Typography sx={{ textAlign: "center" }}>
-                            C++
+                            Java
                           </Typography>
                         </div>
                       </Grid>
@@ -103,14 +107,26 @@ const Details = () => {
                             </div>
                           </Grid>
                           <Grid md={4} lg={4}>
-                            <div x={{ ml: 4 }}>
+                            <div x={{ ml: 4, mt: 2 }}>
                               <Typography>Difficulty Level</Typography>
                             </div>
-                            <div></div>
+                            <div style={{ display: "flex", marginTop: "1rem" }}>
+                              <div>
+                                <NetworkCellIcon />
+                              </div>
+                              <div style={{ marginLeft: "10px" }}>Medium</div>
+                            </div>
                           </Grid>
                           <Grid md={4} lg={4}>
                             <div>
                               <Typography>Course Rating</Typography>
+                              <div style={{ marginTop: "1rem" }}>
+                                <StarOutlineIcon />
+                                <StarOutlineIcon />
+                                <StarOutlineIcon />
+                                <StarOutlineIcon />
+                                <StarOutlineIcon />
+                              </div>
                             </div>
                             <div></div>
                           </Grid>
@@ -125,14 +141,26 @@ const Details = () => {
           <Grid lg={4} md={4}>
             <Box>
               <div>
-                <img 
-                  style={{ height: "auto", width: "100%",marginLeft:"30px" }}
+                <img
+                  style={{ height: "auto", width: "100%", marginLeft: "30px" }}
                   src="https://i.ibb.co/gMrDY2J/Image.png"
                   alt=""
                 />
               </div>
-              <div><Typography>Free</Typography></div>
-              <div><Button sx={{ml:3}} style={{height:'50px' ,width:"100%"}} variant="contained"><h4>registar  now</h4></Button></div>
+              <div>
+                <Typography variant="h5" style={{ marginLeft: "2rem" }}>
+                  Free
+                </Typography>
+              </div>
+              <div>
+                <Button
+                  sx={{ ml: 3 }}
+                  style={{ height: "50px", width: "100%" }}
+                  variant="contained"
+                >
+                  <h4>registar now</h4>
+                </Button>
+              </div>
             </Box>
           </Grid>
         </Grid>
