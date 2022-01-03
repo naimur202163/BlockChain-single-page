5cbc75aa18840afa3bea653dddb495fe4d4d2270
unknown <you@example.com>
2022-01-03 08:59:43 +0600

    matrial Ui added

--- pages/Components/Details/Details.js
+++ pages/Components/Details/Details.js
@@ -1,15 +1,25 @@
-import { Grid } from "@mui/material";
+import { Box, Button, Container, Grid, Typography } from "@mui/material";
 import React from "react";
 
 const Details = () => {
   return (
     <div>
-      <Grid container spacing={2}>
-        <Grid lg={8} md={8}>
-          <h3>THis is fior</h3>
+      <Container sx={{ mt: 4 }}>
+        <Grid container spacing={2}>
+          <Grid lg={8} md={8}>
+            <Box>
+              <div>
+                <Button>Click here</Button>
+              </div>
+              {/* Heading Text */}
+              <div>
+                <Typography>Blockchain Simplified</Typography>
+              </div>
+            </Box>
+          </Grid>
+          <Grid lg={8} md={8}></Grid>
         </Grid>
-        <Grid lg={8} md={8}></Grid>
-      </Grid>
+      </Container>
     </div>
   );
 };
